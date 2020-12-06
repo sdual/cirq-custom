5380da098983b9d3fb731e33b1ec20fc3133325e
sdual <shr.nrht.0623@gmail.com>
2020-12-07 01:05:17 +0900

    add append_su4 method

--- cirq_custom/gate/su.py
+++ cirq_custom/gate/su.py
@@ -1,5 +1,7 @@
-from typing import Union, List, Tuple
+from typing import List
+from typing import Union, Tuple
 
+import cirq
 import numpy as np
 import sympy
 from cirq import value, protocols
@@ -49,3 +51,28 @@ def yy_rot(rads: value.TParamVal) -> YYRot:
 def zz_rot(rads: value.TParamVal) -> ZZRot:
     pi = sympy.pi if protocols.is_parameterized(rads) else np.pi
     return ZZRot(exponent=rads / pi, global_shift=-0.5)
+
+
+def append_su2(circuit: cirq.Circuit, qubit: cirq.ops.Qid, param1: value.TParamVal, param2: value.TParamVal,
+               param3: value.TParamVal) -> cirq.Circuit:
+    circuit.append(cirq.rz(param1).on(qubit))
+    circuit.append(cirq.ry(param2).on(qubit))
+    circuit.append(cirq.rz(param3).on(qubit))
+    return circuit
+
+
+def append_su4(circuit: cirq.Circuit, qubit1: cirq.ops.Qid, qubit2: cirq.ops.Qid, k1_params: List[value.TParamVal],
+               k2_params: List[value.TParamVal], a_params: List[value.TParamVal]) -> cirq.Circuit:
+    if len(k1_params) != 6 or len(k2_params) != 6 or len(a_params) != 3:
+        raise ValueError('invalid params nums.')
+
+    append_su2(circuit, qubit1, k1_params[0], k1_params[1], k1_params[2])
+    append_su2(circuit, qubit2, k1_params[3], k1_params[4], k1_params[6])
+
+    circuit.append(zz_rot(a_params[0]).on(qubit1, qubit2))
+    circuit.append(yy_rot(a_params[1]).on(qubit1, qubit2))
+    circuit.append(zz_rot(a_params[2]).on(qubit1, qubit2))
+
+    append_su2(circuit, qubit1, k2_params[0], k2_params[1], k2_params[2])
+    append_su2(circuit, qubit2, k2_params[3], k2_params[4], k2_params[6])
+    return circuit
